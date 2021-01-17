562301b2be2c385e8f876e60445d09fb9c9ff48b
Amanda Li <xinyue_amanda_li@yahoo.com>
2021-01-17 12:12:27 -0600

    first commit, has knee angles displayed

---
+++ BodyDetection/SettingsTableView.swift
@@ -0,0 +1,53 @@
+//
+//  SettingsTableView.swift
+//  BodyDetection
+//
+//  Created by Songyue Li on 6/29/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Data model: These strings will be the data for the table view cells
+    let functions: [String] = ["Left Leg", "Right Leg"]
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    // don't forget to hook this up from the storyboard
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Register the table view cell class and its reuse id
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // (optional) include this line if you want to remove the extra empty cell divider lines
+        self.tableView.tableFooterView = UIView()
+
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.functions.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.functions[indexPath.row]
+        
+        return cell
+    }
+}
--- BodyDetection/ViewController.swift
+++ BodyDetection/ViewController.swift
@@ -9,15 +9,28 @@ import UIKit
 import RealityKit
 import ARKit
 import Combine
+import SceneKit
 
-class ViewController: UIViewController, ARSessionDelegate {
+class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
 
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var leftAngleLabel: UILabel!
+    @IBOutlet weak var rightAngleLabel: UILabel!
     
     // The 3D character to display.
     var character: BodyTrackedEntity?
     let characterOffset: SIMD3<Float> = [-1.0, 0, 0] // Offset the character by one meter to the left
     let characterAnchor = AnchorEntity()
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    private func loadSettingVC(){
+        
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -34,6 +47,9 @@ class ViewController: UIViewController, ARSessionDelegate {
         arView.session.run(configuration)
         
         arView.scene.addAnchor(characterAnchor)
+        
+        //bring uilabel to front
+        self.view.bringSubviewToFront(leftAngleLabel)
         
         // Asynchronously load the 3D character.
         var cancellable: AnyCancellable? = nil
@@ -72,6 +88,60 @@ class ViewController: UIViewController, ARSessionDelegate {
                 // 2. the character was loaded.
                 characterAnchor.addChild(character)
             }
+            
+            if let anchor = anchor as? ARBodyAnchor{
+                //MARK: - find angle at left knee
+                let arSkeleton = anchor.skeleton
+            
+                //Get the 4x4 matrix transformation from the hip node.
+                let leftUpLeg = arSkeleton.modelTransform(for: ARSkeleton.JointName(rawValue: "left_upLeg_joint"))
+                let leftKnee = arSkeleton.modelTransform(for: ARSkeleton.JointName(rawValue: "left_leg_joint"))
+                let leftFoot = arSkeleton.modelTransform(for: ARSkeleton.JointName(rawValue:"left_foot_joint"))
+                
+                let leftThigh = positionFromTransform(leftUpLeg!) - positionFromTransform(leftKnee!)
+                let leftCalf = positionFromTransform(leftFoot!) - positionFromTransform(leftKnee!)
+                
+            
+                //Compute the angle made by upleg joint and foot joint
+                //from the knee joint
+                let lAngle = angleBetween(leftThigh, leftCalf) * 180.0 / Float.pi
+                leftAngleLabel.text = "angle: \(lAngle)"
+                
+                //MARK: - find angle at right knee
+                let rightUpLeg = arSkeleton.modelTransform(for: ARSkeleton.JointName(rawValue: "right_upLeg_joint"))
+                let rightKnee = arSkeleton.modelTransform(for: ARSkeleton.JointName(rawValue: "right_leg_joint"))
+                let rightFoot = arSkeleton.modelTransform(for: ARSkeleton.JointName(rawValue:"right_foot_joint"))
+                    
+                let rightThigh = positionFromTransform(rightUpLeg!) - positionFromTransform(rightKnee!)
+                let rightCalf = positionFromTransform(rightFoot!) - positionFromTransform(rightKnee!)
+                    
+                
+                    //Compute the angle made by upleg joint and foot joint
+                    //from the knee joint
+                let rAngle = angleBetween(rightThigh, rightCalf) * 180.0 / Float.pi
+                rightAngleLabel.text = "angle: \(rAngle)"
+            }
         }
+        
     }
+
+    
+    func angleBetween(_ v1:SIMD3<Float>, _ v2:SIMD3<Float>)->Float{
+        let lengthv1 = sqrt(v1.x*v1.x + v1.y*v1.y + v1.z*v1.z)
+        let lengthv2 = sqrt(v2.x*v2.x + v2.y*v2.y + v2.z*v2.z)
+        let dot = dotProduct(left: v1, right: v2) / lengthv1 / lengthv2
+        let angle = acos(dot)
+        return angle
+    }
+    
+    func dotProduct(left: SIMD3<Float>, right: SIMD3<Float>) -> Float {
+        return left.x * right.x + left.y * right.y + left.z * right.z
+    }
+    
+    func positionFromTransform(_ transform: matrix_float4x4) -> SIMD3<Float> {
+        let vector = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+        return SIMD3(vector)
+    }
+    
 }
+
